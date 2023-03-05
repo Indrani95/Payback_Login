751098d3619a8c46e4c470d2495015b1f55a35b7
Indrani Muvvala <indraniyadav95@gmail.com>
2023-03-05 13:14:54 +0530

    Update ConfigClass.py

--- features/utility/ConfigClass.py
+++ features/utility/ConfigClass.py
@@ -1,5 +1,5 @@
 class ConfigClass:
 
-    filePath = "\\drivers\\chromedriver.exe"
+    filePath = "drivers\\chromedriver.exe"
     url = "https://www.payback.in/"
-    dataFile = "\\datafiles\\Data.xlsx"
+    dataFile = "datafiles\\Data.xlsx"
